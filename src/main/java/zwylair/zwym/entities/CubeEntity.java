7003699ebe8960c1317208d626caf788fd9c7c28
Zwylair <zwylair@gmail.com>
2024-04-23 03:36:22 +0300

    make cube floating around owner

--- src/main/java/zwylair/zwym/entities/CubeEntity.java
+++ src/main/java/zwylair/zwym/entities/CubeEntity.java
@@ -2,10 +2,40 @@ package zwylair.zwym.entities;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
+
 public class CubeEntity extends MobEntity {
+    private PlayerEntity owner;
+
     public CubeEntity(EntityType<? extends MobEntity> type, World world) {
         super(type, world);
+    }
+
+    public void setOwner(PlayerEntity player) {
+        this.owner = player;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (this.owner != null) {
+            double distance = 1.70; // Расстояние от игрока, на котором должен находиться энтити
+            double angle = this.getWorld().getTime() * 0.5; // Угол вращения, можно изменить скорость вращения
+            double offsetX = Math.cos(angle) * distance;
+            double offsetZ = Math.sin(angle) * distance;
+            double playerX = this.owner.getX();
+            double playerY = this.owner.getY();
+            double playerZ = this.owner.getZ();
+            this.updatePosition(playerX + offsetX, playerY, playerZ + offsetZ);
+            // Вычисляем угол между энтити и игроком
+            double angleToPlayer = Math.atan2(offsetZ, offsetX);
+            // Преобразуем радианы в градусы и добавляем 90 градусов (чтобы энтити смотрел в сторону игрока)
+            float yaw = (float) Math.toDegrees(angleToPlayer) + 90.0f;
+            this.setYaw(yaw);
+        } else {
+            this.setOwner(this.getWorld().getClosestPlayer(this, 3.0));
+        }
     }
 }
--- src/main/kotlin/zwylair/zwym/items/ElectricShieldScroll.kt
+++ src/main/kotlin/zwylair/zwym/items/ElectricShieldScroll.kt
@@ -10,6 +10,8 @@ import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import zwylair.zwym.ZwyM
 import zwylair.zwym.ModObject.ModItem
+import zwylair.zwym.entities.CubeEntity
+import zwylair.zwym.entities.ModEntities
 import zwylair.zwym.itemgroups.ItemGroups
 
 
@@ -20,6 +22,12 @@ class ElectricShieldScroll : ModItem(FabricItemSettings().maxCount(8)) {
     override fun use(world: World?, player: PlayerEntity?, hand: Hand?): TypedActionResult<ItemStack> {
         val itemStack = player!!.getStackInHand(hand)
         if (world!!.isClient) return TypedActionResult.fail(itemStack)
+
+        val entity = CubeEntity(ModEntities.CubeEntityType, world)
+        entity.setOwner(player)
+        entity.setPosition(player.x + 0.5, player.y, player.z + 0.5)
+        world.spawnEntity(entity)
+        itemStack.decrement(1)
 
         return TypedActionResult.success(itemStack)
     }
